abf846ef968d569cded4aad25930856e347f22b6
esthefaniagv <esthefaniagv@gmail.com>
2023-05-08 18:05:12 -0400

    primer commit todos los archivos

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,57 @@
 const cipher = {
-  // ...
+  encode: function (offset, message) {
+    if (typeof offset !== 'number' || typeof message !== 'string') {
+      throw new TypeError('Los argumentos no son de tipo correcto');
+    }
+    let resultado = "";
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //para calcular posicion de la letra dentro del abecedario con numero hasta el 25 o menor (negativo)
+    const clave = ((offset % 26) + 26) % 26;
+
+    if (message) {
+      for (let i = 0; i < message.length; i++) {
+        if (alphabet.indexOf(message[i]) !== -1) {
+          const position = (alphabet.indexOf(message[i]) + clave) % 26;
+          resultado += alphabet[position];
+        } else resultado += message[i];
+      }
+    }
+    return resultado;
+  },
+
+  decode: function (offset, message) {
+    if (typeof offset !== 'number' || typeof message !== 'string') {
+      throw new TypeError('Los argumentos no son de tipo correcto');
+    }
+    let resultado = "";
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //para calcular posicion de la letra dentro del abecedario con numero hasta el 25 o menor (negativo)
+    const clave = ((offset % 26) - 26) % 26;
+
+    if (message) {
+      for (let i = 0; i < message.length; i++) {
+        if (alphabet.indexOf(message[i]) !== -1) {
+          const position = (alphabet.indexOf(message[i]) - clave) % 26;
+          resultado += alphabet[position];
+        } else resultado += message[i];
+      }
+    }
+    return resultado;
+  },
 };
 
 export default cipher;
+
+// const cipher ={
+//     encode: function(offset, string){
+//       let userWord = "";
+//       for(let i=0; i< string.lenght; i++){
+//         let indexPalabra = string.charCodeAt(i);
+//         if (indexPalabra >= 65 && location <= 90){
+//           indexPalabra = ((indexPalabra - 65 + offset)% 26) + 65;
+//         }
+//         userWord += string.fromChardCode(indexPalabra);
+//       }
+//       return userWord;
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+
+function start() {
+  document.getElementById("userInput").addEventListener("keyup", function () {
+    this.value = this.value.toUpperCase();
+  });
+}
+
+start();
+
+document.getElementById("code").addEventListener("click", function () {
+  const message = document.getElementById("userInput").value;
+  const offset = document.getElementById("rangeSelector").value;
+  const palabra = cipher.encode(offset, message);
+  const msgEncode = document.getElementById("userOutput");
+
+  msgEncode.value = palabra;
+});
+
+document.getElementById("decode").addEventListener("click", function () {
+  const message = document.getElementById("userInput").value;
+  const offset = document.getElementById("rangeSelector").value;
+  const palabra = cipher.decode(offset, message);
+  const msgEncode = document.getElementById("userOutput");
+
+  msgEncode.value = palabra;
+});
+
+// codeMessage.addEventListener("click", function () {
+//   let string = document.getElementById("userInput").value;
+//   let offset = document.getElementById("rangeSelector").value;
+//   let userWord = document.getElementById("userOutput");
+
+//   userWord.textContent = cipher.encode(offset, string);
+// });
